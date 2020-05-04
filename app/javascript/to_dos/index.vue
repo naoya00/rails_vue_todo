d54757729e83a17ce8901024cc91c825a679dc1f
naoya00 <bsk470@gmail.com>
2020-05-04 14:15:40 +0900

    add checkbox追加

--- app/javascript/to_dos/index.vue
+++ app/javascript/to_dos/index.vue
@@ -3,6 +3,14 @@
     :data="toDos"
     style="width: 100%">
     <el-table-column
+      prop="finished">
+      <template v-slot="scope">
+        <el-checkbox
+          v-model="scope.row.finished"
+           @change="updateToDo(scope.row.id, scope.row.finished)"></el-checkbox>
+      </template>
+    </el-table-column>
+    <el-table-column
       prop="date"
       label="Date"
       width="180">
@@ -52,6 +60,14 @@ export default {
           this.toDos = reject(this.toDos, ['id', id]);
         }
       });
+    },
+  updateToDo(id, finished) {
+  axios.patch('/api/v1/to_dos/' + id, {to_do: {finished: finished}})
+    .then(res => {
+      if (res.status === 200) {
+        console.log(res)
+          }
+      })
     }
   }
 }
